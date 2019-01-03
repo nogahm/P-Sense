27f7348724bcf77b88de9f0e223e0214754c04dd
לינוי שמחה צרפתי <sarfatyl@auth.ad.bgu.ac.il>
2019-01-03 12:27:24 +0200

    kk

--- server/moduls/users.js
+++ server/moduls/users.js
@@ -5,8 +5,6 @@ var morgan = require('morgan');
 var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 var DButilsAzure = require('../DButil');
 
-
-/*----------------------------------------------------------------------------------------------------------------*/
 //login -get user information from server - use POST because GET is not secured.
 
 router.post('/login', function (req, res) {
@@ -36,7 +34,7 @@ router.post('/login', function (req, res) {
         }
     }).catch(function (err) { res.status(400).send(err); });
 });
-/*----------------------------------------------------------------------------------------------------------------*/
+
 
 //register-save new user record in server.
 
@@ -59,8 +57,6 @@ router.post('/register', function (req, res) {     //Add User
     });
 });
 
-
-/*----------------------------------------------------------------------------------------------------------------*/
 //get user info {username, firstName, lastName, age, gender, email}
 router.get('/Reginfo', function (req, res) {
     var userName = req.params.userName;
@@ -70,17 +66,16 @@ router.get('/Reginfo', function (req, res) {
         }).catch(function (err) { res.status(400).send(err); });
 });
 
-
-/*----------------------------------------------------------------------------------------------------------------*/
-//save new not registered user 
-
+//NotRegUser - save new not registered user and return userId
+//firsName, lastName, age, gender, email
 router.post('/NotRegUser', function (req, res) {     //Add User
+    var username = req.body.userName;
+    var password = req.body.password;
     var firstName = req.body.firstName;
     var lastName = req.body.lastName;
     var age = req.body.age;
     var gender = req.body.gender;
     var email = req.body.email;
-   
     
     query1 = "INSERT INTO NotRegUsers VALUES ('" + firstName + "','" + lastName + "','" + age + "','" + gender + "','" + email  + "')";
 
@@ -92,21 +87,14 @@ router.post('/NotRegUser', function (req, res) {     //Add User
 });
 
 
-/*----------------------------------------------------------------------------------------------------------------*/
-// //GET not register user info {firsName, lastName, age, gender, email}
-// router.get('/NotRegUser', function (req, res) {
-//     var userId = req.params.userId;
-//     DButilsAzure.execQuery("SELECT a.firstName, a.lastName, a.age, a.gender, a.email FROM NotRegUsers a  WHERE userId='"+userId+"'"")
-//         .then(function (result) {
-//             res.send(result);
-//         }).catch(function (err) { res.status(400).send(err); });
-// });
+
 
 //return user email in order to send him a mail with his password
-router.get('/passwordRecovery', function (req, res) {
-    var userId = req.params.userId;
-    DButilsAzure.execQuery("SELECT a.email FROM NotRegUsers a  WHERE userId='"+userId+"'"")
-        .then(function (result) {
-            res.send(result);
-        }).catch(function (err) { res.status(400).send(err); });
+router.post('/passwordRecovery', function (req, res) {    
+    var userName = req.body.userName;
+    query1 = "SELECT a.email FROM Users a WHERE userName='"+userName+"'";
+    DButilsAzure.execQuery(query1).then(function (result) {  
+    }).catch(function (err) {
+        res.status(400).send(err);
+    });
 });
