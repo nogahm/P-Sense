4fd21a541add7b7baf4c786c62d7877f56ab382f
נגה מלמד כהן <nogahm@auth.ad.bgu.ac.il>
2019-03-05 15:52:36 +0200

    a

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use('/Users', users);
 app.use('/Questions', Questions);
 app.use('/Tests',Tests );
 
-var port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, function () {
     console.log('Example app listening on port ' + port);
 });
