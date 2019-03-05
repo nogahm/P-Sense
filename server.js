717b345a6f36877e81f6145d36b6d1c5795dab4e
נגה מלמד כהן <nogahm@auth.ad.bgu.ac.il>
2019-03-05 16:03:14 +0200

    a

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ var jwt = require('jsonwebtoken');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
+app.use(express.static('app'));
 var superSecret= "secret";
 //complete your code here
 
